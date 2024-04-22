4fe6f4a21e7b81a9d0931ba8ca54ab951433bba0
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-04-22 17:11:14 +0600

    added footer

--- src/MainRoute.jsx
+++ src/MainRoute.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
 
 const MainRoute = () => {
     return (
         <div className="">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
